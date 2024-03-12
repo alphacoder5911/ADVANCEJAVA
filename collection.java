58db49b0949239205e1f97788238ef338926a848
alphacoder5911 <lalgeshreyas@gmail.com>
2024-03-13 00:57:53 +0530

    POINTS ABT COLLECTIONS AND ITS USE AND IMPORTANCE

---
+++ collection.java
@@ -0,0 +1,17 @@
+//package ADVANCEJAVA;
+public class collection{
+public static void main(String [] args)
+{
+//COLLECTION IS A FRAME WORK IN JAVA THAT CONTAINS READYMADE CLASSES AND INTERFACES SO THAT WE CAN WRITE
+//EFFECIENTLY AND FASTER ;
+/* 
+ * NEED:1]FOR EFFECIENT STORAGE.
+ * 2]BETTER MEMORY MANAGEMENT 
+ * 
+ * WAYS TO ACCESS COLLECTIONS:
+ * IMPORT java.util.collections package 
+ * arraylist,set,stack and hashmap arre some colleections
+ */
+
+}
+}
